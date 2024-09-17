892f3cf037ac2a267e1dba279f5bfa15a8e23a4e
Emmanuel Okon <emmaudo27@gmail.com>
2024-09-17 17:05:31 +0100

    added site meta data

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-mono">
+      <h1>Welcome to Airbnb Home page</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
