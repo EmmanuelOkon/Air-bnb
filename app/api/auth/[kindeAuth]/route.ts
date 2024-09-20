4faeeab6451ce6998e1948ec06b4551b61932b64
Emmanuel Okon <emmaudo27@gmail.com>
2024-09-20 09:05:10 +0100

    added kinde issuer to route.ts

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,12 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+// export const GET = handleAuth();
+
+if (!process.env.KINDE_ISSUER_URL) {
+  throw new Error(
+    "The environment variable 'KINDE_ISSUER_URL' is required. Set it in your .env file"
+  );
+}
+
+export const GET = handleAuth({
+  issuerUrl: process.env.KINDE_ISSUER_URL,
+});
