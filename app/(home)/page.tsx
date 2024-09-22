b11f02687ed931faa691388bd1a682e74af15901
Emmanuel Okon <emmaudo27@gmail.com>
2024-09-22 19:59:23 +0100

    suspense map filter

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,20 @@
 // import Link from "next/link";
 // import Image from "next/image";
-import MapFilter from "@/components/shared/MapFilter";
+import * as React from "react";
+import dynamic from "next/dynamic";
+// import MapFilter from "@/components/shared/MapFilter";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const MapFilter = dynamic(() => import("@/components/shared/MapFilter"), {
+  suspense: true,
+});
 
+export default function Home() {
   return (
     <div className="container mx-auto font-sans">
-      <MapFilter />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <MapFilter />
+      </React.Suspense>
       <Button>Click me</Button>
     </div>
   );
